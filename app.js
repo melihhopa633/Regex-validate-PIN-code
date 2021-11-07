dc8343e09232c0850355bcdbbabcb9bfc0ae90ef
melihhopa633 <75516781+melihhopa633@users.noreply.github.com>
2021-11-07 21:51:30 +0300

    Add files via upload
    ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
    
    If the function is passed a valid PIN string, return true, else return false.
    Examples (Input --> Output)
    
    "1234"   -->  true
    "12345"  -->  false
    "a234"   -->  false

---
+++ app.js
@@ -0,0 +1,18 @@
+function validatePIN(pin) {
+  var intArr = pin.split("");
+  let a = 0;
+  for (let i = 0; i < intArr.length; i++) {
+    if (!isNaN(intArr[0]) == true) {
+      a++;
+    }
+  }
+  if (a === intArr.length) {
+    if (a === 4 || a === 6) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
