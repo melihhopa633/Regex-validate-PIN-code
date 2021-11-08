bce0170b5f191cc4a056cbd55fbd2eede9a3a5b8
melihhopa633 <75516781+melihhopa633@users.noreply.github.com>
2021-11-08 16:24:40 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+function validatePIN(pin) {
+  var intArr = pin.split("");
+  let a = 0;
+  for (let i = 0; i < intArr.length; i++) {
+    if (intArr[i] === " ") {
+      break;
+    } else if (!isNaN(intArr[i]) === true) {
+      a++;
+    }
+  }
+  if (a === intArr.length) {
+    if (a === 4 || a === 6) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+validatePIN("1233");
